e7ac57fe41d1f72be64330e555524a9e509d6e8d
Gabriel Caceres <gacs86@gmail.com>
2023-04-29 17:33:25 -0400

    Update inputrc

--- inputrc.sh
+++ inputrc.sh
@@ -1,8 +1,13 @@
 # ignore case on TAB autocompletion
 set completion-ignore-case on
 
-# possibly interesting https://www.topbug.net/blog/2017/07/31/inputrc-for-humans/
-
+# Details here: https://www.topbug.net/blog/2017/07/31/inputrc-for-humans/
 # display possible completions using different colors to indicate their file type
 set colored-stats on
 set colored-completion-prefix
+set colored-stats On
+set completion-prefix-display-length 5
+set mark-symlinked-directories On
+set show-all-if-ambiguous On
+set show-all-if-unmodified On
+set visible-stats On
