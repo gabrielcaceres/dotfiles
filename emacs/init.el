2d26959a99e27a0b856fc63e0312bbb10f70dba1
Gabriel Caceres <gacs86@gmail.com>
2023-05-29 13:55:00 -0400

    Start creating emacs init file

---
+++ emacs/init.el
@@ -0,0 +1,14 @@
+
+
+;;;;;; possible modes to look more into
+
+;; track recent files
+;; (recentf-mode 1)
+
+;; Change location of custom file (from UI)
+;; (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+;; (load custom-file 'noerror 'nomessage)
+
+;; Revert buffers when the underlying file had changed
+;; (global-auto-revert-mode 1)
+
