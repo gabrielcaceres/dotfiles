ffd0e61d8cf46782fead6cf40b050219f216b1c8
Gabriel Caceres <gacs86@gmail.com>
2023-05-29 20:32:16 -0400

    Add some extra options and reorder

--- inputrc.sh
+++ inputrc.sh
@@ -1,12 +1,13 @@
-# ignore case on TAB autocompletion
-set completion-ignore-case on
-
-# Details here: https://www.topbug.net/blog/2017/07/31/inputrc-for-humans/
+# Options details here: https://man.archlinux.org/man/readline.3
 # display possible completions using different colors to indicate their file type
-set colored-stats on		       # Completion colors file/dir types
-set colored-completion-prefix on       # Color matched prefix
+set blink-matching-paren On
+set colored-completion-prefix On       # Color matched prefix
+set colored-stats On		       # Completion colors file/dir types
+set completion-ignore-case on          # Ignore case on TAB autocompletion
 set completion-prefix-display-length 5 # If longer completion, show `...`
-set mark-symlinked-directories on      # Add / if syslink maps to dir
-set show-all-if-ambiguous off          # Show all right away with partial
-set show-all-if-unmodified off	       # Show all right away without partial
-set visible-stats on	               # Completion appends file/dir type char 
+set mark-symlinked-directories On      # Add / if syslink maps to dir
+set revert-all-at-newline On
+set show-all-if-ambiguous Off          # Show all right away with partial
+set show-all-if-unmodified On	       # Show all right away without partial
+set skip-completed-text on
+set visible-stats On	               # Completion appends file/dir type char 
