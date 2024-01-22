97b180ba5ddf34b5a7833b42a034af57d34c9b4b
Gabe Caceres <gacs86@gmail.com>
2024-01-22 13:08:00 -0500

    Set up git prompt and bash completion

--- bashrc.sh
+++ bashrc.sh
@@ -155,9 +155,10 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
         __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
-        GIT_PROMPT_ONLY_IN_REPO=1
+        GIT_PROMPT_ONLY_IN_REPO=0
         source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
     fi
+    [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]] && . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
 else
     # Unknown.
     echo "Unknown OS"
